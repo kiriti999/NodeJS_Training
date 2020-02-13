13cb7546636546a3c9cf55be43ff6674154bca1b
Kiriti K <kiriti.k999@gmail.com>
2020-02-14 00:43:08 +0530

    m

--- task4.js
+++ task4.js
@@ -1,13 +1,15 @@
-const http = require('http'); // Import Node.js core module
+// Import Node.js core modules
+const http = require('http');
 const url = require("url");
 const fs = require('fs');
 
-var server = http.createServer(function (req, res) {   //create web server
+//create web server
+var server = http.createServer(function (req, res) {
     const query = url.parse(req.url, true);
     const filePath = JSON.parse(JSON.stringify(query.query)).path;
     const route = query.pathname;
 
-    // Default
+    // Default route
     if (req.url == '/') {
         res.writeHead(200, { 'Content-Type': 'text/html' });
         res.write('<html><body><p>This is home Page.</p></body></html>');
@@ -17,22 +19,52 @@ var server = http.createServer(function (req, res) {   //create web server
     else if (route == "/readFile" && req.method === 'GET') {
         fs.readFile(filePath, 'utf8', (err, text) => {
             if (err) {
-                res.status(500).send({ error: err.message });
+                sendErrorMessage(err);
             } else {
+                res.writeHead(200);
                 res.write(text);
                 res.end();
             }
         });
     }
-    else if (req.url == "/admin") {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write('<html><body><p>This is admin Page.</p></body></html>');
+    else if (route == "/writeFile" && req.method === 'POST') {
+
+        res.writeHead(200);
+        fs.access(file, fs.constants.F_OK, (err) => {
+            console.log(`${file} ${err ? 'does not exist' : 'exists'}`);
+            if (err) {
+                sendErrorMessage(err);
+            } else {
+                fs.writeFile(filePath, 'sample updated', (err, data) => {
+                    if (err) {
+                        sendErrorMessage(err);
+                    } else {
+                        console.log('file has been saved');
+                    }
+                });
+            }
+        });
+
         res.end();
     }
-    else
-        res.end('Invalid Request!');
+    else if (route == "/deleteFile" && req.method === 'GET') {
+        fs.unlink(filePath, (err, file) => {
+            if (err) {
+                sendErrorMessage(err);
+            } else {
+                res.writeHead(200);
+                res.write(`${filePath}: deleted`);
+                res.end();
+            }
+        });
+    }
+    res.end('Invalid Request!');
 });
 
 server.listen(5000); //6 - listen for any incoming requests
 
 console.log('Node.js web server at port 5000 is running..')
+
+function sendErrorMessage(err) {
+    res.status(500).send({ error: err.message });
+}
