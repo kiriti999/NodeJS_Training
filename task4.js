32659759db44fd08897de8fe585cd77fe9c15ff6
Kiriti Komaragiri <Kiriti_Komaragiri@epam.com>
2020-02-13 18:40:42 +0530

    c

--- task4.js
+++ task4.js
@@ -1,34 +1,36 @@
-var http = require('http'); // Import Node.js core module
-var querystring = require('querystring');
+const http = require('http'); // Import Node.js core module
+const url = require("url");
+const fs = require('fs');
 
 var server = http.createServer(function (req, res) {   //create web server
-    if (req.url == '/') { //check the URL of the current request
+    const query = url.parse(req.url, true);
+    const filePath = JSON.parse(JSON.stringify(query.query)).path;
+    const route = query.pathname;
 
-        // set response header
+    // Default
+    if (req.url == '/') {
         res.writeHead(200, { 'Content-Type': 'text/html' });
-
-        // set response content    
         res.write('<html><body><p>This is home Page.</p></body></html>');
         res.end();
-
     }
-    else if (req.url == "/readFile") {
-        console.log('query ', querystring.parse('path'));
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write('<html><body><p>readFile</p></body></html>');
-        res.end();
-
+    // Read
+    else if (route == "/readFile" && req.method === 'GET') {
+        fs.readFile(filePath, 'utf8', (err, text) => {
+            if (err) {
+                res.status(500).send({ error: err.message });
+            } else {
+                res.write(text);
+                res.end();
+            }
+        });
     }
     else if (req.url == "/admin") {
-
         res.writeHead(200, { 'Content-Type': 'text/html' });
         res.write('<html><body><p>This is admin Page.</p></body></html>');
         res.end();
-
     }
     else
         res.end('Invalid Request!');
-
 });
 
 server.listen(5000); //6 - listen for any incoming requests
